b9eabed8dfd76c35866cc95833bde5069b80efc3
wanjugucode <karobiaanastasia.com>
2021-05-24 09:10:21 +0300

    code

---
+++ js.js
@@ -0,0 +1,28 @@
+var person={
+    name:"Anastasia",
+    age:23
+}
+
+    let {name,age}=person
+    console.log(person.name)
+
+
+async function number(){
+    throw 27
+}
+function number(){
+    Promise.resolve(27)
+
+};
+
+
+let Element={
+    element1:"wind",
+    element2:"earth",
+    element3:"fine",
+}
+Element.element4="air"
+console.log(Element)
+delete Element.element2
+console.log(Element)
+
