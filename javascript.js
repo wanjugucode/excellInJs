586ca45f45869b28e0d55546539400559b7eba88
wanjugucode <karobiaanastasia.com>
2021-05-24 09:56:09 +0300

    code

---
+++ javascript.js
@@ -0,0 +1,215 @@
+    // Applying all we've learnt On Car
+
+    var carName="Mazida";//Hoisting
+    // var is a mutable variable 
+    //carName is a variable that is storage of Mazda
+   //Mazda has datatype string 
+   // var carName="Mazda" is a statement
+   //this is Global scope as CarName can be accessed everywher
+let x=function(){     //Anonymous function and declaring a function as a variable x
+var carname2="Subaru";
+console.log(carname2); 
+console.log(carName);
+}
+//console.log(carname);// carName2 is local scope which means that it can't be accessed outside the block
+x()
+
+
+
+
+
+
+function car(carname,make,capacity){  // function creation ' function name is Car(named function) with parameters name,make,capacity and keyword function 
+console.log(carname); // output statement
+}
+car("Mazda","Toyota",5) // arguments Mazda,Toyota and 5
+      // datatype  of 5 is a number
+       // datatype of MAzda and Toyota is string
+       //function call
+
+
+
+
+
+(function IIFE(){
+const capacity=5;           // const is a variable that can't be reassigned or change
+console.log(capacity);
+} () )                 // Immediately invoked function 
+
+let  maxfare=(capacity,fare)=>capacity*fare;
+console.log(maxfare(5,100))               //Arrow function
+                         //let variable which can be changed or not
+
+
+
+
+function start() {
+let carStart = "Voooooooooo"
+console.log(carStart)            // closure where function stop has ability to remember what is in start and it is also a nested function
+function stop() {
+let carStop = carStart + " proooo"  //concatination
+console.log(carStop)
+
+}
+stop()
+}
+start();
+
+function buyCar() {
+const price = 50000000
+console.log(price)
+}
+function sellCar(sellingPrice, callback) {
+setTimeout(() => {
+console.log("Call back Function");
+}, 3000);                                // setTimeout
+console.log(sellingPrice)
+
+}
+sellCar(2000000, buyCar());   // Call back function : buyCar is used as an argument of sellCar
+
+
+class Car {     //class creation called Car
+constructor(registration, model) {
+this.registration = registration;   //this keyword that define an object they belong to
+this.model = model;
+this.carInfo = function () {    // method which is function used as a property of object
+return " Our car has" + " " + this.registration + "" + "number  " + " and" + " " + this.model
+}
+Car.prototype.refillPetrol = 40;    // using prototype to add an other object property
+}
+}
+let car1 = new Car("KC5678", "cx567") // creation of object car1 using new keyword 
+console.log(car1.registration)     //access
+console.log(car1.carInfo())
+console.log(car1.refillPetrol)
+
+let promise=new Promise((resolve,reject)=>{   //promises
+let carCrushes=true;      //boolean datatype
+if(carCrushes){
+resolve("pay back")
+}
+else{
+reject("Buy another one")    //conditional if else statement
+}
+
+}).then((message)=>{
+console.log('Insurance will pay back');
+}).catch((message)=>{
+console.log("can't be compessated")
+})
+async function cleanCar(){                //Asynchronous function
+let y=setInterval(() => {        //setinterval and clear it
+console.log("Let'st start cleaning");
+clearInterval(y);
+}, 2000);
+console.log("Wuw!! We are done")
+}
+cleanCar()
+
+
+let car = {         //object car
+fname: 'mercedes',   //object properties
+lname: "Benz"
+}
+console.log(car.fname); // Dot method to access an object's property
+console.log(car['lname']); //Bracket method to access an object's property
+let { fname, lname } = car    //Object destructuring
+console.log(fname);
+
+
+
+let { fname, ...rest } = car    //using rest keyword 
+console.log(rest.lname);
+console.log(fname)
+
+for (let [key, value] of Object.entries(car)) {    //for loop
+console.log(`${key}:${value}`)
+}
+
+
+for (key in car) {     //for in loop
+console.log(key)
+}
+
+
+
+let car = ["Mini", "Bus"];
+let [fname, lname] = car     // Array destructuring
+console.log(fname);
+
+
+for(name of car){   //for of loop
+console.log(name);
+}
+
+
+
+export function Speed(value){  //export module
+return speed*3;
+}
+
+
+let carNum=0;
+while(carNum<5){   //While loop
+console.log(carNum);
+carNum++;
+}
+
+
+
+let carNumber=6;   //do while loop
+do{   
+console.log(carNumber);
+carNumber--;
+}
+while(carNumber>0);
+
+
+
+
+let carId='100';
+let carId2=number(carId)
+console.log(typeof(carId)); // Explicit coersion
+
+
+ let carId2=carId + '';
+console.log(carId2) // implicit coersion
+
+
+
+let carSpeed="50";
+let carspeed2=50;
+console.log(carSpeed==carspeed2);//loosely equal to, Compares only values
+console.log(carSpeed===carspeed2);//strictly equal to,Compares both values and types
+console.log(carSpeed!=carspeed2);//Not loosely equal to 
+console.log(carSpeed!==carspeed2);//Not strictly equal to
+
+
+
+
+var carN=40;      //using switch case
+switch(true){
+case carN>60:
+console.log("Perfect");
+break;
+default:
+cconsole.log("Not enough")
+}
+
+
+
+function ternaryOp(){        //Ternary operator
+car=="car"? console.log("true"): console.log("false") 
+}
+ternaryOp()
+
+
+
+
+var strict=function(){   //using strict mode which will give us an error 
+"use strict",
+a=30;
+console.log(a)
+}
+strict()       //function as a value
--- recap.js
+++ recap.js
@@ -1,190 +1,190 @@
-let arr=[23,45,67,89,89,89];
+// let arr=[23,45,67,89,89,89];
 
-let sum=0;
-for(var x=4;x<arr.length;x++){//sum of Array
-    sum+=arr[x];
-}
-console.log(arr.length)
-console.log(sum)
-console.log(sum/arr.length) //average
+// let sum=0;
+// for(var x=4;x<arr.length;x++){//sum of Array
+//     sum+=arr[x];
+// }
+// console.log(arr.length)
+// console.log(sum)
+// console.log(sum/arr.length) //average
 
-// conditions
+// // conditions
 
-var x=20
-var y=60
-if(x==10){
-    console.log(x)
-}
-else if(y>50){
-    console.log(y)
+// var x=20
+// var y=60
+// if(x==10){
+//     console.log(x)
+// }
+// else if(y>50){
+//     console.log(y)
 
-}
-else{
-    console.log('false')
-}
-
-
-
-var age=26
-var gender="female"
-if(gender=="female"){ 
-if (age<18)
-{
-console.log("child")
-}
-else{ 
-console.log("sorry is a man")
-}}
-
-
- //1
-for( var i=1;i<=100;i++ )
-{
-  if(i%3===0 && i%5===0)
-  {
-    console.log(i +"FizzBuzz");
-  }
-  else if (i%3 ===0) 
-  {
-    console.log(i+ "Fizz");
-  }
-  else if (i%5 ===0) 
-  {
-    console.log(i+"Buzz");
-  }
-  else
-  {
-    console.log(i);
-  }
-}
+// }
+// else{
+//     console.log('false')
+// }
 
 
 
- // 2
-a=-6;
-b=-9;
-c=-1;
-d= 7;
-f=-4;
-if(a>b && a>c && a>c && a>d && a>f ){
-    console.log(a);
-}
-else if(b>a && b>c && b>d && b>f){
-    console.log(b);
-}
-else if(c>a && c>b && c>d&& c>f){
-    console.log(c);
-}
-else if(d>a && d>b && d>c&& c>f){
-    console.log(d);
-}
-else {
-    console.log(f);
-}
+// var age=26
+// var gender="female"
+// if(gender=="female"){ 
+// if (age<18)
+// {
+// console.log("child")
+// }
+// else{ 
+// console.log("sorry is a man")
+// }}
 
-// 3
-var students = [['John', 85], ['James', 78], ['juliet', 92], ['Job', 50],
- ['jane', 66]];
 
-var averagemarks=0
-
-for (var i=0; i < students.length; i++) {
-    averagemarks += students[i][1];
-    var avg = (averagemarks/students.length);
-    }
-    console.log("average grade: " + (averagemarks)/students.length);
-
-    if (avg < 60){
-      console.log("Grade : F");      
-      } 
-
-    else if (avg < 70) {
-            console.log("Grade : D"); 
-              } 
-
-    else if (avg < 80) 
-         {
-
-         console.log("Grade : C"); 
-    } 
-     else if (avg < 90) {
-            console.log("Grade : B"); 
-    } 
-     else if (avg < 100) {
-            console.log("Grade : A"); 
-}
+//  //1
+// for( var i=1;i<=100;i++ )
+// {
+//   if(i%3===0 && i%5===0)
+//   {
+//     console.log(i +"FizzBuzz");
+//   }
+//   else if (i%3 ===0) 
+//   {
+//     console.log(i+ "Fizz");
+//   }
+//   else if (i%5 ===0) 
+//   {
+//     console.log(i+"Buzz");
+//   }
+//   else
+//   {
+//     console.log(i);
+//   }
+// }
 
 
 
-//4
-for (var x=0; x<=15; x++) {
-  if (x === 0) {
-    console.log(x +  " is even");
-  }
-    else if (x % 2 === 0) {
-      console.log(x + " is even");   
-}
-else {
-  console.log(x + " is odd");
-}}
+//  // 2
+// a=-6;
+// b=-9;
+// c=-1;
+// d= 7;
+// f=-4;
+// if(a>b && a>c && a>c && a>d && a>f ){
+//     console.log(a);
+// }
+// else if(b>a && b>c && b>d && b>f){
+//     console.log(b);
+// }
+// else if(c>a && c>b && c>d&& c>f){
+//     console.log(c);
+// }
+// else if(d>a && d>b && d>c&& c>f){
+//     console.log(d);
+// }
+// else {
+//     console.log(f);
+// }
 
- //5
-var sums=0;
-for(i=1;i<1000;i++){
-  if(i%3==0||i%5==0){
-sums+=i
-  }
-}
-console.log(sums)
+// // 3
+// var students = [['John', 85], ['James', 78], ['juliet', 92], ['Job', 50],
+//  ['jane', 66]];
+
+// var averagemarks=0
+
+// for (var i=0; i < students.length; i++) {
+//     averagemarks += students[i][1];
+//     var avg = (averagemarks/students.length);
+//     }
+//     console.log("average grade: " + (averagemarks)/students.length);
+
+//     if (avg < 60){
+//       console.log("Grade : F");      
+//       } 
+
+//     else if (avg < 70) {
+//             console.log("Grade : D"); 
+//               } 
+
+//     else if (avg < 80) 
+//          {
+
+//          console.log("Grade : C"); 
+//     } 
+//      else if (avg < 90) {
+//             console.log("Grade : B"); 
+//     } 
+//      else if (avg < 100) {
+//             console.log("Grade : A"); 
+// }
 
 
- //6
 
-function student(id,latestCallback){
-  latestCallback();
-}
-function latestStudentList(){
-  console.log('hello')
-}
-student(1,latestStudentList)
+// //4
+// for (var x=0; x<=15; x++) {
+//   if (x === 0) {
+//     console.log(x +  " is even");
+//   }
+//     else if (x % 2 === 0) {
+//       console.log(x + " is even");   
+// }
+// else {
+//   console.log(x + " is odd");
+// }}
 
- //7
-function leapyear(year){
- return(year%100===0)?(year%400===0):(year%4===0)
-}
-console.log(leapyear(2021));
-console.log(leapyear(2020));
-console.log(leapyear(2016));
-console.log(leapyear(2000));
-console.log(leapyear(1900));
+//  //5
+// var sums=0;
+// for(i=1;i<1000;i++){
+//   if(i%3==0||i%5==0){
+// sums+=i
+//   }
+// }
+// console.log(sums)
 
- //8
-function readStatus(){
-  library = [ 
-{
-       author: 'Bill Gates', 
-    title: 'The Road Ahead', 
-    readingStatus: true }, 
-{
-     author: 'Steve Jobs',
-     title: 'Walter Isaacson',
-      readingStatus: true }, 
-{ 
-      author: 'Suzanne Collins', 
-      title: 'Mockingjay',
-   readingStatus: false }
-  ];
-  for (var i = 0; i < library.length; i++){
-    var book =library[i].title+" " +'by'+" " +library[i].author ;
-    if (library[i].readingStatus) {
-      console.log("Already read " + book);
-    } else
-    {console.log("You still need to read " + book);
 
-    var book="'"+library[i].readStatus
-  }
- }
-}
-readStatus();
+//  //6
+
+// function student(id,latestCallback){
+//   latestCallback();
+// }
+// function latestStudentList(){
+//   console.log('hello')
+// }
+// student(1,latestStudentList)
+
+//  //7
+// function leapyear(year){
+//  return(year%100===0)?(year%400===0):(year%4===0)
+// }
+// console.log(leapyear(2021));
+// console.log(leapyear(2020));
+// console.log(leapyear(2016));
+// console.log(leapyear(2000));
+// console.log(leapyear(1900));
+
+//  //8
+// function readStatus(){
+//   library = [ 
+// {
+//        author: 'Bill Gates', 
+//     title: 'The Road Ahead', 
+//     readingStatus: true }, 
+// {
+//      author: 'Steve Jobs',
+//      title: 'Walter Isaacson',
+//       readingStatus: true }, 
+// { 
+//       author: 'Suzanne Collins', 
+//       title: 'Mockingjay',
+//    readingStatus: false }
+//   ];
+//   for (var i = 0; i < library.length; i++){
+//     var book =library[i].title+" " +'by'+" " +library[i].author ;
+//     if (library[i].readingStatus) {
+//       console.log("Already read " + book);
+//     } else
+//     {console.log("You still need to read " + book);
+
+//     var book="'"+library[i].readStatus
+//   }
+//  }
+// }
+// readStatus();
  
   
